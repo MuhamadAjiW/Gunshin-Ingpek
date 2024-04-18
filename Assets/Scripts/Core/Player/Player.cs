253b48a4463c61594b93fefa2a4e20ccef44f479
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2024-04-19 00:21:31 +0700

    refactor: foldering and class name

--- Assets/Scripts/Core/Player/Player.cs
+++ Assets/Scripts/Core/Player/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : AccompaniableCombatant{
+public class Player : AccompaniableCombatant {
     // Attributes
     private PlayerAnimationController animationController;
     private PlayerMovementController movementController;
