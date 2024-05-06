2c53ef7518ce2888bc5d4bab452475eadcf5f819
Chiquita Ahsanunnisa <16521248@mahasiswa.itb.ac.id>
2024-05-06 11:45:12 +0700

    feat: add flag in stateffect

--- Assets/_Scripts/Library/DataTypes/StatEffects.cs
+++ Assets/_Scripts/Library/DataTypes/StatEffects.cs
@@ -10,13 +10,15 @@ public class StatEffect
     public int statType;
     public int opType;
     public float value;
+    public StatEffectFlag statFlag;
 
     // Constructor
-    public StatEffect(string description, int statType, int opType, float value)
+    public StatEffect(string description, int statType, int opType, float value, StatEffectFlag statFlag = StatEffectFlag.NONE)
     {
         this.description = description;
         this.statType = statType;
         this.opType = opType;
         this.value = value;
+        this.statFlag = statFlag;
     }
 }
---
+++ Assets/_Scripts/Library/Enum/StatEffectFlag.cs
@@ -0,0 +1,6 @@
+public enum StatEffectFlag
+{
+    // Stat Effect Flags because some effects need to be applied only once
+    NONE,
+    INC_SPEED_ORB,
+}
