d6ac8c19bdd0201f8aab57a27abe829f53200a3e
Fatih20 <fnri39@protonmail.com>
2024-05-11 14:17:30 +0700

    fix: remove shop and save button

--- Assets/UI/InGame/PauseMenu/PauseMenuController.cs
+++ Assets/UI/InGame/PauseMenu/PauseMenuController.cs
@@ -21,23 +21,23 @@ public class PauseMenuController : InGameUIScreenController
             GameController.stateController.PopState();
         });
 
-        Button saveButton = rootElement.Query<Button>("Save");
-        // Insert condition on which player can save the game
-        if (false)
-        {
-            saveButton.style.display = DisplayStyle.None;
-        }
-        saveButton.RegisterCallback((ClickEvent evt) =>
-        {
-            Debug.Log("Save Button clicked");
-            GameSaveManager.Instance?.SaveGame();
-        });
+        // Button saveButton = rootElement.Query<Button>("Save");
+        // // Insert condition on which player can save the game
+        // if (false)
+        // {
+        //     saveButton.style.display = DisplayStyle.None;
+        // }
+        // saveButton.RegisterCallback((ClickEvent evt) =>
+        // {
+        //     Debug.Log("Save Button clicked");
+        //     GameSaveManager.Instance?.SaveGame();
+        // });
 
-        Button shopButton = rootElement.Query<Button>("Shop");
-        shopButton.RegisterCallback((ClickEvent evt) =>
-        {
-            GameController.Instance.stateController.PushState(GameState.SHOPPING);
-        });
+        // Button shopButton = rootElement.Query<Button>("Shop");
+        // shopButton.RegisterCallback((ClickEvent evt) =>
+        // {
+        //     GameController.Instance.stateController.PushState(GameState.SHOPPING);
+        // });
 
         Button returnToMainMenuButton = rootElement.Query<Button>("ReturnToMainMenu");
 
